623462b3e302176a84e9457da677bf01275627ec
defiz <devvv3ze@gmail.com>
2026-07-14 19:17:48 +0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,9 @@
+print("Say anything to DefizAI. Type 'exit' to exit.")
+
+while True:
+    token = input("You : ").lower()
+
+    if token == "exit":
+        break
+    else:
+        print("DefizAI : ...")
